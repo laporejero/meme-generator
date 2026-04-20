9dce2b1a16da7836f27cae915c9d955c92410714
laporejero <laporejero1297@gmail.com>
2026-04-20 10:13:38 +0800

    Update MainContent.jsx
    added state for the meme info

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,25 +1,33 @@
+import { useState } from 'react'
+
 export default function MainContent() {
+    const [meme, setMeme] = useState({
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor",
+        imageUrl: "http://i.imgflip.com/1bij.jpg"
+    })
+
     return (
         <main>
             <div className="form">
                 <label>Top Text
                     <input 
                         type="text" 
-                        placeholder="One does not simply"
+                        placeholder={meme.topText}
                         name="topText"
                     />
                 </label>
                 <label>Bottom Text 
                     <input 
                         type="text"
-                        placeholder="Walk into Mordor"
+                        placeholder={meme.bottomText}
                         name="bottomText"
                     />
                 </label>
                 <button>Get a new meme image 🖼</button>
             </div>
             <div className="meme">
-                <img src="http://i.imgflip.com/1bij.jpg" />
+                <img src={meme.imageUrl} />
                 <span className="top">One does not simply</span>
                 <span className="bottom">Walk into Mordor</span>
             </div>
